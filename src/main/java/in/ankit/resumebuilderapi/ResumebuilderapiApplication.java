4bf29aa0ad0879424b2e8324efac782a5a829439
AnkitV15 <av5120634@gmail.com>
2025-12-14 00:05:12 +0530

    feat: Update Mail Config

--- src/main/java/in/ankit/resumebuilderapi/ResumebuilderapiApplication.java
+++ src/main/java/in/ankit/resumebuilderapi/ResumebuilderapiApplication.java
@@ -3,7 +3,9 @@ package in.ankit.resumebuilderapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration.class
+})
 public class ResumebuilderapiApplication {
 
     public static void main(String[] args) {
